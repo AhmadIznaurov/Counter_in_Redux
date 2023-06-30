764faeda3331437a6cd82c6da5cc261b8ffae9fd
AhmadIznaurov <111181823+AhmadIznaurov@users.noreply.github.com>
2023-06-30 22:44:17 +0300

    first

---
+++ src/Section/Section.js
@@ -0,0 +1,44 @@
+import React from "react";
+import d from './Section.module.css';
+import {useDispatch, useSelector} from "react-redux";
+
+
+const Section = () => {
+    const count = useSelector( (state) => state);
+    const dispatch = useDispatch();
+
+
+
+    const increment = () => {
+        dispatch( {
+            type: 'increment'
+        })
+    };
+
+    const decrement = () => {
+        dispatch({
+            type: 'decrement'
+        })
+    };
+
+    const reset = () => {
+        dispatch({
+            type: 'reset'
+        })
+    };
+
+    return (
+        <div className={d.section}>
+            <div className={d.clock}>
+            </div>
+            <aside className={d.text_zero}><span>{count}</span></aside>
+            <div className={d.section_buttons}>
+                <button onClick={increment}>Увеличить</button>
+                <button onClick={decrement}>Уменьшить</button>
+                <button onClick={reset}>Сбросить</button>
+            </div>
+        </div>
+    )
+}
+
+export default Section;
