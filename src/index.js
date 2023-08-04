8b5028d6e28c4d7f244fa8798388e4875c628645
AhmadIznaurov <111181823+AhmadIznaurov@users.noreply.github.com>
2023-08-05 01:46:33 +0300

    localStorage

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ import App from './App';
 import {Provider} from "react-redux";
 import {createStore} from "redux";
 
+const initialState = 0
 
-
-const reducer = (state = 0, action) => {
+const reducer = (state = initialState, action) => {
     switch (action.type) {
         case 'increment':
-            return state + 3
+            return state + 1
         case 'decrement':
             return state > 1 ? state -2 : 0
         case 'reset':
@@ -20,14 +20,22 @@ const reducer = (state = 0, action) => {
     }
 }
 
-const store = createStore(reducer)
+
+
+const preloadedState = JSON.parse(localStorage.getItem('counter')) // The JSON parsing method helps to get the key using localStorage to find the required key
+console.log(preloadedState) // This log process only shows the result of compiling
+
+const store = createStore(reducer, preloadedState ? preloadedState : initialState) // This condition returns preloadedState else 0
+
+store.subscribe(() =>  {                 // We use subscribe to call a localstorage in it
+    localStorage.setItem('counter', store.getState())
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
+
         <Provider store={store}>
             <App />
         </Provider>
-    </React.StrictMode>
 
 );
